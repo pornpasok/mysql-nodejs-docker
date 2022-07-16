f68bd7acfb1092105aff6bd3d9f69ca83185e5b9
pornpasok robinhood.in.th <pornpasok@robinhood.in.th>
2022-07-16 12:25:52 +0700

    add code

---
+++ db/testdump.sql
@@ -0,0 +1,28 @@
+CREATE TABLE Student(
+       student_id INT PRIMARY KEY AUTO_INCREMENT, 
+       student_name VARCHAR(60), 
+       student_age INT
+);
+INSERT INTO Student(
+       student_name, 
+       student_age) 
+VALUES(
+       "ลุ่มน้ำโขงออนไลน์", 
+        21
+);
+INSERT INTO Student(
+       student_name, 
+       student_age) 
+VALUES(
+       "Jakkrit", 
+        23
+);
+INSERT INTO Student(
+       student_name, 
+       student_age) 
+VALUES(
+       "weaq", 
+        24
+);
+ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY 'password'; 
+flush privileges;
